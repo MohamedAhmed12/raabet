9e68ae4bab03fcc66f5a13705f5d9a35b68edf6e
m_gad <mgad@unifonic.com>
2025-03-21 22:52:45 +0200

    feat(shared-components): create dashboard pages main title

---
+++ src/app/(protected)/dashboard/admin/components/MainTitle.tsx
@@ -0,0 +1,16 @@
+export const MainTitle = ({
+  title,
+  subTitle,
+}: Readonly<{
+  title: string;
+  subTitle: string;
+}>) => (
+  <div className="flex flex-col items-center py-11 px-[22]">
+    <span className="text-[32px] text-deep-blue-gray font-normal capitalize leading-none">
+      {title}
+    </span>
+    <span className="mt-[12px] text-[20px] text-[#727272] leading-none">
+      {subTitle}
+    </span>
+  </div>
+);
