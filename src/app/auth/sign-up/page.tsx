89ceb4841105237517ed0f3797c1c4326838449b
Mazen-Ahmed12 <mazena083@gmail.com>
2025-04-05 00:17:36 +0200

    fix(verify): fix verify page ui

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -22,7 +22,7 @@ export default function SignUp() {
       <div className="flex-1 relative hidden bg-muted lg:block">
         <img
           src="https://d1ym67wyom4bkd.cloudfront.net/assets/bundles/db9264c8bc4385992e0f73e2eb736dbc6cb1dfaf/graphics/signup-graphic.png"
-          alt="Sign-in"
+          alt="Sign-up"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
