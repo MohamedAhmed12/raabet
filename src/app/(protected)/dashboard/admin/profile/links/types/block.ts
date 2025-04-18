25729d1b665ff3ceaf192abcfd42ec989c827635
m_gad <mgad@unifonic.com>
2025-04-18 15:01:20 +0200

    add blocksTitle to blocks type file

--- src/app/(protected)/dashboard/admin/profile/links/types/block.ts
+++ src/app/(protected)/dashboard/admin/profile/links/types/block.ts
@@ -1,16 +1,17 @@
-import { AudioBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/AudioBlock";
-import { EmailBtnBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/EmailBtnBlock";
-import { FileBtnBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/FileBtnBlock";
-import { GroupBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/GroupBlock";
-import { ImageBtnBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/ImageBtnBlock";
-import { SeparatorBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/SeparatorBlock";
-import { TextBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/TextBlock";
-import { URLBtnBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/URLBtnBlock";
-import { VideoBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/VideoBlock";
+import {AudioBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/AudioBlock";
+import { BtnBlock } from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/BtnBlock/page";
+import {EmailBtnBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/EmailBtnBlock";
+import {FileBtnBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/FileBtnBlock";
+import {GroupBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/GroupBlock";
+import {ImageBtnBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/ImageBtnBlock";
+import {SeparatorBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/SeparatorBlock";
+import {TextBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/TextBlock";
+import {URLBtnBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/URLBtnBlock";
+import {VideoBlock} from "../components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/VideoBlock";
 
 export const blocks = {
   text: TextBlock,
-  urlBtn: URLBtnBlock,
+  urlBtn: BtnBlock,
   emailBtn: EmailBtnBlock,
   fileBtn: FileBtnBlock,
   imageBtn: ImageBtnBlock,
@@ -21,3 +22,15 @@ export const blocks = {
 };
 
 export type BlockType = keyof typeof blocks;
+
+export const blockTitle = {
+  text: "TextBlock",
+  urlBtn: "new button",
+  emailBtn: "new button",
+  fileBtn: "new button",
+  imageBtn: "new button",
+  separator: "SeparatorBlock",
+  audio: "AudioBlock",
+  video: "VideoBlock",
+  group: "GroupBlock",
+};
