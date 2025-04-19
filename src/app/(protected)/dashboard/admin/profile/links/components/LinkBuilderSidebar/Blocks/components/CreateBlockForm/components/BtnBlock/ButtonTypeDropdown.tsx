f9d7a134d31ffb6014fdfc2c642e2179189aa246
m_gad <mgad@unifonic.com>
2025-04-19 12:46:09 +0200

    Create ButtonTypeDropdown.tsx

---
+++ src/app/(protected)/dashboard/admin/profile/links/components/LinkBuilderSidebar/Blocks/components/CreateBlockForm/components/BtnBlock/ButtonTypeDropdown.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import * as React from "react";
+
+import {Button} from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {BlockType} from "../../../../../../../../links/types/block";
+import {Block} from "@/app/store/use-link-store";
+
+const linkOptions: {
+  // id: number;
+  label: string;
+  value: string;
+  inputType: "text" | "file";
+  placeholder?: string | undefined;
+}[] = [
+  {
+    label: "link",
+    value: "url",
+    inputType: "text",
+    placeholder: "https://example.com/",
+  },
+  {
+    label: "email",
+    value: "email",
+    inputType: "text",
+    placeholder: "hello@example.com",
+  },
+  {label: "image", value: "image", inputType: "file"},
+  {label: "file", value: "file", inputType: "file"},
+];
+
+export const ButtonTypeDropdown = ({
+  onChange,
+}: {
+  onChange: (key: keyof Block, val: string) => void;
+}) => {
+  const [btnType, setBtnType] = React.useState("url");
+  const [test, setTest] = React.useState();
+  const selectedType = React.useMemo(
+    () => linkOptions.find((opt) => opt.value === btnType) || linkOptions[0],
+    [btnType]
+  );
+
+  const handleTypeSelected = (value: string) => {
+    setBtnType(value);
+    onChange("type", value);
+  };
+  return (
+    <div className="flex flex-col font-inherit gap-5">
+      <div className="w-1/3">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Button Type</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-56 font-noto-sans"
+            align="start"
+            alignOffset={3}
+          >
+            <DropdownMenuLabel>Button Type</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuRadioGroup
+              value={btnType}
+              onValueChange={handleTypeSelected}
+            >
+              {linkOptions.map((option) => (
+                <DropdownMenuRadioItem
+                  key={option.label}
+                  value={option.value}
+                  className="capitalize"
+                >
+                  {option.label}
+                </DropdownMenuRadioItem>
+              ))}
+            </DropdownMenuRadioGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      {btnType && (
+        <div className="flex flex-col gap-2">
+          <Label>{selectedType.label}</Label>
+
+          <Input
+            id={btnType}
+            type={selectedType.inputType}
+            placeholder={selectedType.placeholder}
+            value={test}
+            className="mb-[14px]"
+            onChange={(e) => console.log(e)}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
