f77b78038ca98657f39364454e8addc868ce5422
Mazen-Ahmed12 <mazena083@gmail.com>
2025-04-22 18:37:22 +0200

    fix(social): removing any comments and logs

--- src/app/(protected)/dashboard/admin/profile/links/components/DashbaordSortableList/page.tsx
+++ src/app/(protected)/dashboard/admin/profile/links/components/DashbaordSortableList/page.tsx
@@ -1,18 +1,3 @@
-// import { useLinkStore } from "@/app/store/use-link-store";
-// import { SortableItem } from "./SortableItem";
-
-// export const DashbaordSortableList = () => {
-//   const link = useLinkStore((state) => state.link);
-
-//   return (
-//     <ul className="list w-full">
-//       {link?.socials?.map((social, index) => (
-//         <SortableItem key={social.id} social={social} index={index} />
-//       ))}
-//     </ul>
-//   );
-// };
-
 'use client';
 
 import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core';
--- src/app/api/socials/order/route.ts
+++ src/app/api/socials/order/route.ts
@@ -1,12 +1,9 @@
-// /app/api/social/order/route.ts
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function POST(req: Request) {
   try {
     const data = await req.json();
-    console.log('Received data11111:', data); // Log the incoming data
-
     const updatePromises = data.map((item: { id: string; order: number }) =>
       prisma.social.update({
         where: { id: item.id },
@@ -16,10 +13,8 @@ export async function POST(req: Request) {
 
     await Promise.all(updatePromises);
 
-    console.log('Order updated successfully22222222');
     return NextResponse.json({ message: 'Order updated' });
   } catch (error) {
-    console.error('Error updating order3333333333333:', error); // Log any errors
     return NextResponse.json({ message: 'Failed to update order', error: error.message }, { status: 500 });
   }
 }
