afcc79c9d01e2bb5de4005a4d09c2bd3e160eb81
m_gad <mgad@unifonic.com>
2025-05-05 21:46:32 +0300

    feat(dashboard): dashboard not found component

---
+++ src/app/[locale]/(protected)/dashboard/admin/not-found.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {Icon} from "@/components/Icon";
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
+import {useTranslations} from "use-intl";
+
+export default function DashboardNotFound() {
+  const t = useTranslations("NotFoundPage");
+
+  return (
+    <div className="flex w-full pt-[55px] pb-[110px] justify-center">
+      <Card className="flex p-11 justify-center items-center bg-trasnparent !shadow-none border-0 text-gray-500">
+        <CardHeader>
+          <Icon name="user" size={150} strokeWidth={1} />
+        </CardHeader>
+        <CardContent className="flex flex-col justify-center items-center text-2xl">
+          <p>{t("something_went_wrong")} </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
