c34c729304d0074c2cbed7ea5064408e7746372e
m_gad <mgad@unifonic.com>
2025-05-15 00:00:43 +0300

    fix build

--- src/app/[locale]/hooks/useDashboardNotFoundRedirect.ts
+++ src/app/[locale]/hooks/useDashboardNotFoundRedirect.ts
@@ -1,9 +1,1 @@
-import { redirect, usePathname } from "next/navigation";
-
-export const useDashboardNotFoundRedirect = () => {
-  const pathname = usePathname();
-
-  // if (pathname.includes("dashboard")) {
-  //   redirect("/dashboard/admin/profile/settings");
-  // }
-};
+export const useDashboardNotFoundRedirect = () => {};
