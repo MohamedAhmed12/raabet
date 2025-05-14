3d61ae5687cb2332438dcc806af73a37b4966f54
m_gad <mgad@unifonic.com>
2025-05-15 00:28:05 +0300

    fix build by specify specific versions for prisma postgress adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,14 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
+import {PrismaPg} from "@prisma/adapter-pg";
+import {PrismaClient} from "@prisma/client";
+import {Pool} from "pg";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const globalForPrisma = global as unknown as {prisma?: PrismaClient};
 const connectionString = process.env.DATABASE_URL!;
 
-const pool = new Pool({ connectionString });
+const pool = new Pool({connectionString});
 const adapter = new PrismaPg(pool);
 
 // @ts-expect-error: [working fine]
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient({adapter});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
