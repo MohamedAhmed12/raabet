a658e32fe76b7faef154c82f9d25ca0a00c9622d
m_gad <mgad@unifonic.com>
2025-05-15 03:24:50 +0300

    fix images url

--- src/app/[locale]/Home/components/TryNow/ImageSection.tsx
+++ src/app/[locale]/Home/components/TryNow/ImageSection.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 
 export const ImageSection = () => {
   return (
-    <div className="flex justify-end h-full w-full md:w-1/2 pt-[80px]">
+    <div className="flex justify-end relative md:w-1/2 pt-[80px]">
       <Image
-        src="https://d1ym67wyom4bkd.cloudfront.net/assets/bundles/db9264c8bc4385992e0f73e2eb736dbc6cb1dfaf/graphics/signup.png"
+        src="/images/colored-links-page-preview.png"
         alt="Hero Image"
-        className="h-auto w-[37vw]"
+        fill
+        className="!h-100 !w-[37vw]"
       />
     </div>
   );
