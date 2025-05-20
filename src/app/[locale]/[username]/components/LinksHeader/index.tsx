b0301f9bac6cea7d38692c107dff488eb23de4c6
Mazen-Ahmed12 <mazena083@gmail.com>
2025-05-20 22:01:11 +0300

    fix(links): fix name appear in iframe and fix in bio

--- src/app/[locale]/[username]/components/LinksHeader/index.tsx
+++ src/app/[locale]/[username]/components/LinksHeader/index.tsx
@@ -23,12 +23,12 @@ export function LinksHeader() {
             link.title_font && `${link.title_font}`
           )}
         >
-          {link?.user?.fullname}
+          {link?.displayname}
         </div>
         {/* bio */}
         <h2
           className={cn(
-            "font-normal text-[13px] leading-[1.3] max-w-[300px] mt-[31px]",
+            "font-normal text-[13px] leading-[1.3] max-w-[300px] mt-[31px] break-words",
             link.text_font && `${link.text_font}`,
             link.header_styles_collapse_long_bio &&
               collapseBio &&
