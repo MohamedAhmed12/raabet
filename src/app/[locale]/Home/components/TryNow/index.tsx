a1d1141c24fc779bb5b7d89a996f6fe17c560813
m_gad <mgad@unifonic.com>
2025-05-29 14:27:38 +0300

    remvoe redundant code

--- src/app/[locale]/Home/components/TryNow/index.tsx
+++ src/app/[locale]/Home/components/TryNow/index.tsx
@@ -1,6 +1,5 @@
 import { Prosbar } from "@/components/Prosbar";
 import { ContentSection } from "./ContentSection";
-import { ImageSection } from "./ImageSection";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 
@@ -9,8 +8,6 @@ export const TryNow = () => {
     <>
       <Prosbar bgColorClass="bg-[#d9acfd]" />
       <div className="flex flex-col-reverse lg:flex-row bg-light-orange">
-        {/* First Child: Image */}
-        {/* <div className="flex justify-center items-center w-full md:w-1/2 flex-1"> */}
         <div className="relative flex justify-end flex-1 md:flex-1/2 min-h-[80vh] overflow-hidden py-8 px-[7vw]">
           <Image
             src="/images/colored-links-page-preview.png"
@@ -23,10 +20,7 @@ export const TryNow = () => {
             )}
           />
         </div>
-        {/* </div> */}
-        {/* <div className="flex justify-center w-full md:w-1/2 px-[7vw] py-[32px] flex-1"> */}
         <ContentSection />
-        {/* </div> */}
       </div>
     </>
   );
