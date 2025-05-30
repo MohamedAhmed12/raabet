8613ed2ba212eee22caec50e8b8a9b99fa2f0d58
m_gad <mgad@unifonic.com>
2025-05-30 23:45:50 +0300

    feat(qr-code): move qr code render logic to separate hook

--- src/app/[locale]/(protected)/dashboard/admin/qr-codes/components/QRCodeCard.tsx
+++ src/app/[locale]/(protected)/dashboard/admin/qr-codes/components/QRCodeCard.tsx
@@ -4,14 +4,13 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Download, Loader2, Trash2 } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { QRCodeCanvas } from "qrcode.react";
 import { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 import { deleteQRCode } from "../actions/deleteQRCode";
 import { useLinkStore } from "@/app/[locale]/store/use-link-store";
 import { QRType } from "@prisma/client";
 import QRCodeStyling, { Options } from "qr-code-styling";
-import QRCodeStylingOptions from "qr-code-styling";
+import { useQRCode } from "../hooks/useQRCode";
 
 export default function QRCodeCard({
   qr,
@@ -20,36 +19,16 @@ export default function QRCodeCard({
   qr: any;
   onDelete: (id: string) => void;
 }) {
-  const QRStylingOptions: Partial<Options> = {
-    width: 150,
-    height: 150,
-    type: "canvas",
-    data: qr.url,
-    shape: "circle",
-    qrOptions: {
-      errorCorrectionLevel: "M",
-      typeNumber: 0,
-    },
-    dotsOptions: {
-      type: "dots",
-      roundSize: true,
-    },
-    cornersSquareOptions: {
-      type: "square",
-    },
-    cornersDotOptions: {
-      type: "dot",
-    },
-    margin: 1,
-  };
-
   const [isDeleting, setIsDeleting] = useState(false);
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const QRCodeInstanceRef = useRef<QRCodeStyling | null>(null);
 
   const t = useTranslations();
   const profile_views = useLinkStore((state) => state.link.profile_views);
-
+  const { canvasRef, handleDownload } = useQRCode({
+    url: qr.url,
+    width: 160,
+    height: 160,
+    onDownload: () => {},
+  });
   const handleDelete = async () => {
     try {
       setIsDeleting(true);
@@ -59,55 +38,6 @@ export default function QRCodeCard({
       toast.error("Failed to delete QR code");
     } finally {
       setIsDeleting(false);
-    }
-  };
-
-  useEffect(() => {
-    try {
-      if (!qr.url) return;
-
-      // Create QR code instance
-      QRCodeInstanceRef.current = new QRCodeStyling(QRStylingOptions);
-
-      // Append to canvas if available
-      if (canvasRef.current && canvasRef.current.children.length === 0) {
-        QRCodeInstanceRef.current.append(canvasRef.current);
-      }
-
-      return () => {
-        if (QRCodeInstanceRef.current) {
-          QRCodeInstanceRef.current = null;
-        }
-        if (canvasRef.current) {
-          canvasRef.current = null;
-        }
-      };
-    } catch (error) {
-      console.error("Failed to create QR code:", error);
-      toast.error("Failed to create QR code");
-    }
-  }, [qr.url]);
-
-  const handleDownload = () => {
-    if (!canvasRef.current) {
-      toast.error("QR code is not available");
-      return;
-    }
-
-    try {
-      // Create QR code instance
-      const downloadQR = new QRCodeStyling({
-        ...QRStylingOptions,
-        width: 600,
-        height: 600,
-      });
-      downloadQR.download({
-        name: `qr-code-${qr.id}`,
-        extension: "png",
-      });
-    } catch (error) {
-      console.error("Failed to download QR code:", error);
-      toast.error("Failed to download QR code");
     }
   };
 
@@ -143,7 +73,7 @@ export default function QRCodeCard({
             <Button
               variant="outline"
               className="cursor-pointer"
-              onClick={handleDownload}
+              onClick={() => handleDownload(qr.id)}
             >
               <Download className="w-4 h-4 " />
             </Button>
@@ -161,13 +91,6 @@ export default function QRCodeCard({
             </Button>
           </div>
         </div>
-        {/* <QRCodeCanvas
-          ref={canvasRef}
-          value={qr.url}
-          size={90}
-          marginSize={1}
-          level="H"
-        /> */}
 
         <div ref={canvasRef} />
       </CardContent>
