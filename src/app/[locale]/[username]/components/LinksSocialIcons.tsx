76130bae8751c5566b91c934c2c2c335d816206b
m_gad <mgad@unifonic.com>
2025-06-15 14:43:29 +0300

    enhance socials icons styling in user link page

--- src/app/[locale]/[username]/components/LinksSocialIcons.tsx
+++ src/app/[locale]/[username]/components/LinksSocialIcons.tsx
@@ -6,43 +6,39 @@ import { useShallow } from "zustand/react/shallow";
 export default function LinksSocialIcons() {
   const link = useLinkStore(useShallow((state) => state.link));
   const socialIconSize = (link?.header_styles_social_icons_size || 0) * 24;
+
   return (
     link?.socials && (
-      <div className="social-icons-container flex mt-[31px] justify-center items-center flex-wrap">
+      <div className="social-icons-container flex mt-[31px] justify-center items-center flex-wrap gap-x-2 font-noto-sans">
         {link?.socials.map((social) => {
           return !social?.icon ? (
-            <div key={social.id} className="w-full"></div>
+            <div key={social.id} className="w-full my-1"></div>
           ) : (
-            <a key={social.id} href={social.url} target="_blank" rel="noopener noreferrer">
-              {!social.label ? (
-                <Icon
-                  name={social.icon}
-                  size={socialIconSize + 24}
-                  // @ts-expect-error: [props distruct in Icon component]
-                  style={{margin: "calc(4.2px + .21em)"}}
-                />
-              ) : (
-                <div
-                  className={cn(
-                    "flex items-center justify-center border-[0.2em] rounded-[2em] border-solid"
-                  )}
-                  style={{
-                    color: link?.general_styles_primary_text_color,
-                    borderColor: link?.general_styles_primary_text_color,
-                  }}
-                  dir="ltr"
-                >
-                  <Icon
-                    name={social.icon}
-                    size={socialIconSize + 24}
-                    // @ts-expect-error: [props distruct in Icon component]
-                    style={{margin: "calc(4.2px + .21em)"}}
-                  />
-                  <span className="text-xs text-center font-semibold mr-2">
+            <a
+              key={social.id}
+              href={social.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mb-3"
+            >
+              <div
+                className={cn(
+                  social.label &&
+                    "flex items-center justify-center border-[0.2em] rounded-[2em] border-solid gap-2 px-2 py-1.5"
+                )}
+                style={{
+                  color: link?.general_styles_primary_text_color,
+                  borderColor: link?.general_styles_primary_text_color,
+                }}
+                dir="ltr"
+              >
+                <Icon name={social.icon} size={socialIconSize + 24} />
+                {social.label && (
+                  <span className="text-xs text-center font-semibold">
                     {social.label}
                   </span>
-                </div>
-              )}
+                )}
+              </div>
             </a>
           );
         })}
