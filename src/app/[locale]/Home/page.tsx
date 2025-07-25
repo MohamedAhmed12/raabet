3eb3624b95333e4939c9648e8acc2359976f638a
m_gad <mgad@unifonic.com>
2025-07-26 02:59:03 +0300

    fix(sentry): make APP_ENV to be publicly available

--- src/app/[locale]/Home/page.tsx
+++ src/app/[locale]/Home/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import AnimatedBar from "./components/AnimatedBar/page";
 import { CentralizePresence } from "./components/CentralizePresence";
@@ -8,13 +8,11 @@ import { PublicContainer } from "@/components/PublicContainer";
 
 export default function Home() {
   const test = () => {
-    throw new Error("Test Sentry 22Break");
+    throw new Error("Test Sentry 22Break" + process.env.NEXT_PUBLIC_APP_ENV);
   };
   return (
     <PublicContainer>
-      <button onClick={() => test()}>
-        aaa
-      </button>
+      <button onClick={() => test()}>aaa</button>
       <CentralizePresence />
       <AnimatedBar />
       <WhyUs />
