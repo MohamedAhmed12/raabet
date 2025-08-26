bdf7ca3e504bdd2a4d059c95f8efdd42702531c7
m_gad <mgad@unifonic.com>
2025-08-26 23:54:49 +0300

    fix(auth): fix eye icon style for auth input fields

--- src/app/[locale]/auth/components/ResetPasswordForm.tsx
+++ src/app/[locale]/auth/components/ResetPasswordForm.tsx
@@ -95,7 +95,7 @@ export default function ResetPasswordForm({
               />
               <button
                 type="button"
-                className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
+                className="absolute end-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
                 onClick={() => setShowPassword(!showPassword)}
               >
                 {showPassword ? (
