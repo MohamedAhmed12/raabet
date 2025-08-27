dbc6cc41cbaf53693d22426519d9720026294392
m_gad <mgad@unifonic.com>
2025-08-27 05:13:14 +0300

    fix main header url display text

--- src/app/[locale]/(protected)/dashboard/admin/profile/links/components/LinkBuilderSidebar/MainHeaderSection.tsx
+++ src/app/[locale]/(protected)/dashboard/admin/profile/links/components/LinkBuilderSidebar/MainHeaderSection.tsx
@@ -9,19 +9,18 @@ export const MainHeaderSection = () => {
   const link = useLinkStore(useShallow((state) => state.link));
 
   const url = link?.qrcodes?.[0]?.url || "";
-  const formatedUserName =
-    url?.replace(`${process.env.NEXT_PUBLIC_BASE_URL}/ar/`, "") || "";
 
   return (
     <div className="flex justify-between h-[55px] px-[22px] py-[11px]">
       <div className="flex items-center justify-between gap-2">
         <Icon name="copy" size={15} />
+
         <a
           href={url}
           target="_blank"
           className="text-dashboard-primary underline text-sm"
         >
-          {`${process.env.NEXT_PUBLIC_BASE_URL_LABEL}/${formatedUserName}`}
+          {url}
         </a>
       </div>
       <ShareBtn />
