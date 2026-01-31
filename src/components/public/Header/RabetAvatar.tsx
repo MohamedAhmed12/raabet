375b31a0ac8ec27b276e056cb6906b48dc066911
m_gad <mgad@unifonic.com>
2026-01-31 02:51:20 +0200

    feat(RabetAvatar): add locale-based alignment for dropdown menu and integrate locale hook

--- src/components/public/Header/RabetAvatar.tsx
+++ src/components/public/Header/RabetAvatar.tsx
@@ -16,6 +16,7 @@ import { useTranslations } from "next-intl";
 import NextLink from "next/link";
 import { useRouter } from "@/i18n/navigation";
 import { useMemo } from "react";
+import { useLocale } from "next-intl";
 
 type SessionUser = {
   id?: {
@@ -29,6 +30,8 @@ export const RabetAvatar = () => {
   const router = useRouter();
   const { data: session } = useSession();
   const t = useTranslations("Header");
+  const locale = useLocale();
+  const isRTL = locale === "ar";
 
   const sessionUser = session?.user as SessionUser | null;
   const user = sessionUser?.id;
@@ -74,32 +77,32 @@ export const RabetAvatar = () => {
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="" align="end">
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">{fullName}</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {email}
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <NextLink
-            href="/dashboard/admin/profile/links"
+      <DropdownMenuContent align={isRTL ? "start" : "end"}>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">{fullName}</p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem asChild>
+            <NextLink
+              href="/dashboard/admin/profile/links"
+              className="cursor-pointer hover:bg-gray-100"
+            >
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              {t("dashboard")}
+            </NextLink>
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={handleLogout}
             className="cursor-pointer hover:bg-gray-100"
           >
-            <LayoutDashboard className="mr-2 h-4 w-4" />
-            {t("dashboard")}
-          </NextLink>
-        </DropdownMenuItem>
-        <DropdownMenuItem
-          onClick={handleLogout}
-          className="cursor-pointer hover:bg-gray-100"
-        >
-          <LogOut className="mr-2 h-4 w-4" />
-          {t("logout")}
-        </DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            {t("logout")}
+          </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
